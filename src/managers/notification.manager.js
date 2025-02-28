75735aa11c3baef46eacc47919eb4924014fae84
Ernest <rabergcobo@gmail.com>
2025-02-28 02:43:54 -0800

    notification managers for later implementation.

---
+++ src/managers/notification.manager.js
@@ -0,0 +1,17 @@
+import { productManager } from "./product.manager.js";
+
+export const notifyProductChange = async () => {
+    try {
+        const products = await productManager.getAllProducts({
+            limit: 50,
+            sort: 'price',
+            sortDirection: -1,
+        });
+        
+
+        return products;
+    } catch (err) {
+        console.error('Error preparing product change notification:', err);
+        throw err;
+    }
+};
