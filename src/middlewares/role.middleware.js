e78e2aeebb1da03850cf52b62c0eb09df9743e54
Ernest <rabergcobo@gmail.com>
2025-04-02 12:15:06 -0700

    RBAC for carts

---
+++ src/middlewares/role.middleware.js
@@ -0,0 +1,30 @@
+export const requireRole = (role) => {
+    return (req, res, next) => {
+    if (!req.user) {
+        return res.status(401).json({ message: "Unauthorized" });
+    }
+
+    if (req.user.role !== role) {
+        return res.status(403).json({ message: "Forbidden: Insufficient privileges" });
+    }
+
+    next();
+    };
+};
+
+export const requireAdminOrOwner = (paramKey = "cid") => {
+    return (req, res, next) => {
+    if (!req.user) {
+        return res.status(401).json({ message: "Unauthorized" });
+    }
+
+    if (req.user.role === "admin") return next(); // Allow admin
+
+    // Allow user if the resource belongs to them
+    if (req.user.cartId === req.params[paramKey]) {
+        return next();
+    }
+
+    return res.status(403).json({ message: "Forbidden: Not your resource" });
+    };
+};
