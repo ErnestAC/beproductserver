622e7be99dc3bee808077e8948ac8b4a59432619
Ernest <rabergcobo@gmail.com>
2025-04-21 18:35:45 -0700

    Ticket DTO bug fixed

--- src/dto/ticket.dto.js
+++ src/dto/ticket.dto.js
@@ -2,8 +2,7 @@
 
 export class TicketDTO {
     constructor(ticket) {
-        this.mongoID = _id; // db mongoID is only used by Mongo, I use tid to identify the cart in the rest of the code.
-        this.tid = ticket.tid; // this is the ticket ID number, the tid is what identifies the carts widely in this app.
+        this.tid = ticket.tid;
         this.amount = ticket.amount;
         this.purchaser = ticket.purchaser;
         this.purchase_datetime = ticket.purchase_datetime;
